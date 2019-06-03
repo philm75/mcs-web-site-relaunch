ca4dd3e738493a5b9ce3ad4ac55b4d8ecd1aefbb
Phil <phil.merrilees@gmail.com>
2019-06-03 23:24:34 +0100

    Service page changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,15 +2,16 @@ import React, { Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 const HomePage = props => {
+    const yearsExp = new Date().getFullYear() - 1996;
     return (
         <Fragment>
             <div className="row">
                 <div className="col-sm-12">
                     <h2 className="mt-4 text-center">What We Do</h2>
-                    <p>We provide bespoke software development as a service to clients, specialising in developing bespoke software solutions for clients in a wide range of business sectors.  We build software solutions that streamline processes, enable a competitive advantage or allow you to connect with customers and business partners.</p>
-                    <p>Our bespoke software solutions meet our clients needs, help generate revenue streams, on time and to budget.</p>
+                    <p>We provide custom software development as a service to clients, specialising in developing custom software solutions for clients in a wide range of business sectors.  We build software solutions that streamline processes, enable a competitive advantage or allow you to connect with customers and business partners.</p>
+                    <p>Our custom software solutions meet our clients needs, help generate revenue streams, on time and to budget.</p>
                     <h2 className="mt-4 text-center">About Us</h2>
-                    <p>Merrilees Consultant Services Ltd was founded in 2011, based in the north east of England.  We have over 20 years experience providing IT consulting and bespoke software development services to UK and international companies in the private and public sectors.</p>
+                    <p>Merrilees Consultant Services Ltd was founded in 2011, based in the north east of England.  We have over {yearsExp} years experience providing IT consulting and bespoke software development services to UK and international companies in the private and public sectors.</p>
                     <p>
                         <Link to="/Contact" className="btn btn-primary btn-lg">
                             Contact us now
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,7 +2,7 @@ import React, { Fragment, Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faDatabase, faServer } from '@fortawesome/free-solid-svg-icons';
 
 class ServicesPage extends Component {
 
@@ -17,6 +17,7 @@ class ServicesPage extends Component {
                 <div className="row">
                     <div className="col-sm-12">
                         <p>Our software design &amp; development services solve business problems, help businesses grow and succeed.  Our software solutions are productive, engaging and robust that modern businesses demand.  We build the right solutions with modern technologies, architectures, frameworks and tools.</p>
+                        <p>We have developed cutting edge software solutions for private and public sector organisations such as the European Patent Office, Nissan Motor Manufacturing and the Marine Management Organisation.</p>
                     </div>
                 </div>
                 <div className="row text-center">
@@ -27,9 +28,10 @@ class ServicesPage extends Component {
                                     <a className="services-icon" href="#webappdev">
                                         <FontAwesomeIcon icon={faCode} />
                                     </a>
-                                    <h4>Web Application Development</h4>
-                                    <p>We build custom web based applications that meet our clients specific business and technical requirements, our service offering is tailored to our clients needs.</p>
-                                    <p>We have many years of experience in developing cutting edge web applications for private and public sector organisations such as the European Patent Office, Nissan Motor Manufacturing and the Marine Management Organisation.</p>
+                                    <h4>Web Application <br />Development</h4>
+                                    <p>
+                                        We build custom web based applications that meet our clients specific business and technical requirements, our service offering is tailored to our clients needs.
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -39,17 +41,32 @@ class ServicesPage extends Component {
                             <div className="services-details">
                                 <div className="single-services">
                                     <a className="services-icon" href="#coder">
-                                        <FontAwesomeIcon icon={faCode} />
+                                        <FontAwesomeIcon icon={faServer} />
                                     </a>
-                                    <h4>Software Development</h4>
+                                    <h4>Enterprise Software <br />Development</h4>
                                     <p>
-                                        This is the software development services....
+                                        We will improve your software infrastructure with well-built microservices, reliable APIs, web services and database integration.
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>                        
-                </div>         
+                    <div className="col-md-4 col-sm-4 col-xs-12">
+                        <div className="about-move">
+                            <div className="services-details">
+                                <div className="single-services">
+                                    <a className="services-icon" href="#coder">
+                                        <FontAwesomeIcon icon={faDatabase} />
+                                    </a>
+                                    <h4>Database <br />Development</h4>
+                                    <p>
+                                        We specialise in designing and creating custom databases that drive business efficiency and help scale your business.  These databases can be cloud based, local or centralised systems.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Fragment> 
         );
     }
